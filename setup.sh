e193cbb2421dfba64a287188e50287cdc9ae118e
Nguyen Tu Nam <60282305+southnt@users.noreply.github.com>
2022-07-26 12:26:03 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update 
+sudo apt-get install git 
+sudo apt-get install neovim 
+
+# media codecs to play various kinds of media files
+sudo apt-get install ubuntu-restricted-extras
