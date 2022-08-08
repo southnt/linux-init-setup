13a5acadd13796bf2d9869fb8bd326bbf37de2d3
Nguyen Tu Nam <60282305+southnt@users.noreply.github.com>
2022-08-09 07:43:02 +0900

    ibus

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# setting vietnamese key
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo apt-get update
+sudo apt-get install ibus ibus-bamboo --install-recommends -y
+ibus-daemon -d
+ibus restart
+# Đặt ibus-bamboo làm bộ gõ mặc định
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
+
 sudo apt-get update 
-sudo apt-get install git 
-sudo apt-get install neovim 
+sudo apt-get install git -y
+sudo apt-get install neovim -y
 
 # media codecs to play various kinds of media files
-sudo apt-get install ubuntu-restricted-extras
+sudo apt-get install ubuntu-restricted-extras -y
