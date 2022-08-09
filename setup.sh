5db2df0d28f082f829b761d8e03806da97b4c484
Nguyen Tu Nam <60282305+southnt@users.noreply.github.com>
2022-08-09 20:36:16 +0900

    swap

--- setup.sh
+++ setup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# Permanently off swap
+sudo swapoff -a && sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
 # setting vietnamese key
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
 sudo apt-get install ibus ibus-bamboo --install-recommends -y
 ibus-daemon -d
 ibus restart
-# Đặt ibus-bamboo làm bộ gõ mặc định
+# set ibus-bamboo as default
 env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
 
-sudo apt-get update 
 sudo apt-get install git -y
+
+# Vim, Neovim
 sudo apt-get install neovim -y
+cp .vimrc ~/
+cp nvim ~/.config/
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # media codecs to play various kinds of media files
 sudo apt-get install ubuntu-restricted-extras -y
