c21f22f23fbdee97cc509012f71d00d2c9ad5be7
Nguyen Tu Nam <60282305+southnt@users.noreply.github.com>
2022-08-09 20:51:00 +0900

    nvim

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ sudo apt-get install git -y
 # Vim, Neovim
 sudo apt-get install neovim -y
 cp .vimrc ~/
-cp nvim ~/.config/
+cp -r nvim ~/.config/
 # install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
